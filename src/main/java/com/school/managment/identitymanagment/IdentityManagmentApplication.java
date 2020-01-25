ce5cfdfb9620c24deddce4e8b03ed129745c4bc5
namorytraore1984 <namorytraore1984@yahoo.fr>
2020-01-25 12:58:53 -0500

    Premier commit du module gestion des identités

---
+++ src/main/java/com/school/managment/identitymanagment/IdentityManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.school.managment.identitymanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/managment/identitymanagment/IdentityManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.managment.identitymanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
